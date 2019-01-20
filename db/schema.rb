2d2e8773f28fb3eff2022fc8057dcae59cecb81e
artiscream <arti_scream@ukr.net>
2019-01-20 15:32:07 +0200

    dependencies on comm

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_19_171444) do
+ActiveRecord::Schema.define(version: 2019_01_20_130726) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "body"
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
