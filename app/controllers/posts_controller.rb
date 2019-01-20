df6843709d46c90e5dc7ddd95c567c0a7bbdd30e
artiscream <arti_scream@ukr.net>
2019-01-20 16:29:22 +0200

    all done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-	before_action :authentificate_user!, except: [:index, :show]
+	before_action :authenticate_user!, except: [:index, :show]
   	
 
 	def index
